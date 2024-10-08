644a65edbd1cd84485957124e2a19a22619b567f
neelgit1010 <neel.binbrain1010@gmail.com>
2024-10-08 18:41:29 +0530

    made new content section

--- App/src/components/Dashboard.jsx
+++ App/src/components/Dashboard.jsx
@@ -1,27 +1,29 @@
-import { useLocation } from 'react-router-dom'
-import Sidebar from './Sidebar'
+import { useLocation } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 const Dashboard = () => {
   const location = useLocation();
-  const {userData} = location.state || {};
-  
+  const { userData } = location.state || {};
+
   if (userData === undefined) {
-    return <h1>No User Data</h1>
+    return <h1>No User Data</h1>;
   }
   return (
     <div>
-        <div className='header'>
-          <h1>Header</h1>
-        </div>
-        <Sidebar />
-        <div className='content'>
-          <h1>Content</h1>
-          <h2>{userData.first_name ?? 'No Name'}</h2>
-          <h2>{userData.email ?? 'No Email'}</h2>
-        </div>
-        
-    </div>
-  )
-}
+      <div className="header">
+        <h1>Header</h1>
+      </div>
 
-export default Dashboard
+      <div className="d-flex w-100">
+        <Sidebar />
+        <div className="content">
+          <h1>Content</h1>
+          <h2>{userData.first_name ?? "No Name"}</h2>
+          <h2>{userData.email ?? "No Email"}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
