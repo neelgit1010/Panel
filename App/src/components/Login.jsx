863a6e7a016052052e528f9f884add6090c141be
neelgit1010 <neel.binbrain1010@gmail.com>
2024-10-19 14:26:58 +0530

    login, register url updated

--- App/src/components/Login.jsx
+++ App/src/components/Login.jsx
@@ -19,7 +19,7 @@ const Login = ({ showPassword, setShowPassword }) => {
     const {email, password } = loginInfo;
     
     try {
-      const response = await axios.post("http://localhost:5000/auth/login", {
+      const response = await axios.post("https://panel-api-server.vercel.app/auth/login", {
         email,
         password,
       });
